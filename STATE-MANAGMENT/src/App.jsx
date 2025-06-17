a5c6476c733324920701150f79c7fdcea7d4d70b
0xtusharganotra <ganotra.vox@gmail.com>
2025-06-18 01:58:01 +0530

    file : Add code for Roll Up props and props drilling
    Added code for props drilling and Roll up props

--- STATE-MANAGMENT/src/App.jsx
+++ STATE-MANAGMENT/src/App.jsx
@@ -1,34 +1,92 @@
-import { useState } from 'react'
+import { useState  } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+ 
 
-  return (
+//   return (
+//     <>
+//       <Light />
+//     </>
+//   )
+// }
+
+// function Light(){
+//   const [islightbulb , setislightbulb] = useState(true);
+//   return(
+//     <>
+//     <LightBulb islightbulb={islightbulb}/>
+//     <LightSwitch islightbulb = {islightbulb} setislightbulb = {setislightbulb} />
+//     </>
+//   )
+// }
+
+
+// function LightBulb({islightbulb}){
+//   return(
+//     <>
+//     {
+//       islightbulb ? <p>Light Bulb On</p> : <p>Ligh Bulb Off</p>
+//     }
+//     </>
+//   )
+// }
+
+// function LightSwitch({islightbulb , setislightbulb}){
+
+//   function triggeronoff(){
+//     setislightbulb(!islightbulb);
+//   }
+//   return(
+//     <div>
+//       <button onClick={triggeronoff}>Toggle Light</button>
+//     </div>
+//   )
+// }
+// export default App
+
+
+function App(){
+  const [Count ,setCount]  = useState(0);
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Increase setCount = {setCount}/>
+    <Decrease setCount = {setCount} />
+    <Value Count = {Count}/>
     </>
+  )
+
+}
+
+function Increase({setCount}){
+  function increasecounter(){
+    setCount(v => v+1);
+  }
+  return(
+    <>
+    <button onClick={increasecounter}>Increase</button>
+    </>
+  )
+}
+
+function Decrease({setCount}){
+  function decreaseCount(){
+    setCount(v => v-1);
+  }
+  return(
+    <>
+    <button onClick={decreaseCount}>Decrease</button>
+    </>
+  )
+}
+
+function Value({Count}){
+  return(
+    <div>
+      Count : {Count};
+    </div>
   )
 }
 
